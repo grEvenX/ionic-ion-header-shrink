d41102eb2bbc0c129bed9c0232b6652dd463a1fa
Even André Fiskvik <eaf@thecometcult.com>
2014-12-17 23:02:18 +0100

    Cleanup unused code
    Making the example a bit clearer by removing redundant code-parts and not used code.

--- ionic.headerShrink.js
+++ ionic.headerShrink.js
@@ -1,12 +1,10 @@
 angular.module('ionic.ion.headerShrink', [])
 
 .directive('headerShrink', function($document) {
-  var fadeAmt;
-
-  var shrink = function(header, content, amt, max) {
-    amt = Math.min(max, amt);
-    fadeAmt = 1 - amt / max;
+  function shrink(header, amt, max) {
     ionic.requestAnimationFrame(function() {
+      var amt = Math.min(max, amt);
+      var fadeAmt = 1 - amt / max;
       header.style[ionic.CSS.TRANSFORM] = 'translate3d(0, -' + amt + 'px, 0)';
       for(var i = 0, j = header.children.length; i < j; i++) {
         header.children[i].style.opacity = fadeAmt;
@@ -17,43 +15,24 @@ angular.module('ionic.ion.headerShrink', [])
   return {
     restrict: 'A',
     link: function($scope, $element, $attr) {
-      var starty = $scope.$eval($attr.headerShrink) || 0;
-      var shrinkAmt;
-
-      var amt;
-
       var y = 0;
-      var prevY = 0;
       var scrollDelay = 0.4;
-
-      var fadeAmt;
-      
       var header = $document[0].body.querySelector('.bar-header');
       var headerHeight = header.offsetHeight;
       
       function onScroll(e) {
         var scrollTop = e.detail.scrollTop;
 
-        if(scrollTop >= 0) {
+        if (scrollTop >= 0) {
           y = Math.min(headerHeight / scrollDelay, Math.max(0, y + scrollTop - prevY));
         } else {
           y = 0;
         }
         console.log(scrollTop);
 
-        ionic.requestAnimationFrame(function() {
-          fadeAmt = 1 - (y / headerHeight);
-          header.style[ionic.CSS.TRANSFORM] = 'translate3d(0, ' + -y + 'px, 0)';
-          for(var i = 0, j = header.children.length; i < j; i++) {
-            header.children[i].style.opacity = fadeAmt;
-          }
-        });
-
-        prevY = scrollTop;
+        shrink(header, y, headerHeight);
       }
-
       $element.bind('scroll', onScroll);
     }
   }
 })
-
